7976e313ddb376e167f45f1d5c0582312b83aada
Sebastian Homeier <s.homeier@bynary.io>
2020-03-29 01:35:11 +0100

    [FE] rename route of AuthModule from `admin` to `auth`

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
-        path: 'admin',
+        path: 'auth',
         loadChildren: () => import('./features/auth/auth.module').then((m) => m.AuthModule)
     },
     {
